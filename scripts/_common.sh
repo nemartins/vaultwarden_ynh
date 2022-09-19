5ac947d381222fc30ca15ea79e3331fb998a06e6
Nelson Martins <nelson.e.v.martins@gmail.com>
2022-09-19 08:11:51 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="libpq5"
 
-pkg_image="vaultwarden/server"
+pkg_image="vaultwarden/server:1c6d1c6522c4b233f0d56bd189e11fe668d4d55cf49690d9629a9a94cfa66a9e"
 
 #=================================================
 # PERSONAL HELPERS
